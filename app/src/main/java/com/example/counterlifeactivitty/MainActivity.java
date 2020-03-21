cf397634d7b9d07d5e8a2871873ffcefdc62098e
Sitamdu <beekramrai22@gmail.com>
2020-03-21 13:09:25 +0545

    first commit

---
+++ app/src/main/java/com/example/counterlifeactivitty/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.counterlifeactivitty;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private TextView textView;
+    public static final String TAG = "MainActivity";
+    public static final String COUNT = "count";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+
+        if(savedInstanceState != null){
+            int count1 = savedInstanceState.getInt(COUNT);
+            count = count1;
+            textView.setText(""+count);
+        }
+    }
+
+    public void countUp(View view) {
+        count++;
+        if(textView != null){
+            textView.setText(Integer.toString(count));
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: ");
+        outState.putInt(COUNT, count);
+    }
+}
